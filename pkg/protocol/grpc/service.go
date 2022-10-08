10355029bb139fd77f1fb38dcb5cb1dfca281cf7
Jamal Kaksouri <jamal.kaksouri@gmail.com>
2022-10-08 23:00:48 +0330

    Update service.go

--- pkg/protocol/grpc/service.go
+++ pkg/protocol/grpc/service.go
@@ -2,22 +2,19 @@ package grpc
 
 import (
 	"context"
-	"flag"
 	"github.com/jamalkaksouri/todoapp-golang-microservice/pkg/api/v1"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/testdata"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 )
 
-var (
-	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
-	certFile = flag.String("cert_file", "", "The TLS cert file")
-	keyFile  = flag.String("key_file", "", "The TLS key file")
-)
+//var (
+//	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+//	certFile = flag.String("cert_file", "", "The TLS cert file")
+//	keyFile  = flag.String("key_file", "", "The TLS key file")
+//)
 
 func RunServer(ctx context.Context, v1API v1.ToDoServiceServer, port string) error {
 	listen, err := net.Listen("tcp", ":"+port)
@@ -26,20 +23,20 @@ func RunServer(ctx context.Context, v1API v1.ToDoServiceServer, port string) err
 	}
 
 	// Configure TLS
-	var opts []grpc.ServerOption
-	if *tls {
-		if *certFile == "" {
-			*certFile = testdata.Path("server1.pem")
-		}
-		if *keyFile == "" {
-			*keyFile = testdata.Path("server1.key")
-		}
-		creeds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
-		if err != nil {
-			log.Fatalf("Failed to generate credentials %v", err)
-		}
-		opts = []grpc.ServerOption{grpc.Creds(creeds)}
-	}
+	//var opts []grpc.ServerOption
+	//if *tls {
+	//	if *certFile == "" {
+	//		*certFile = testdata.Path("server1.pem")
+	//	}
+	//	if *keyFile == "" {
+	//		*keyFile = testdata.Path("server1.key")
+	//	}
+	//	creeds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+	//	if err != nil {
+	//		log.Fatalf("Failed to generate credentials %v", err)
+	//	}
+	//	opts = []grpc.ServerOption{grpc.Creds(creeds)}
+	//}
 
 	// register service
 	server := grpc.NewServer(opts...)
