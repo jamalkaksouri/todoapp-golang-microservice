e93cd45fe412019211f3234cecdebb97ec701c68
Jamal Kaksouri <jamal.kaksouri@gmail.com>
2022-10-08 19:52:30 +0330

    Refactor readme

--- run apps/run client.cmd
+++ run apps/run client.cmd
@@ -1,3 +1,4 @@
 cd ../
 cd cmd/client-grpc
 client-grpc.exe -server=localhost:9090
+pause
--- run apps/run server.cmd
+++ run apps/run server.cmd
@@ -1,3 +1,3 @@
 cd ../
 cd cmd/server
-server.exe -grpc-port=9090 -db-host=localhost:3306 -db-user=admin -db-password=root -db-schema=todo
+server.exe -grpc-port=9090 -db-host=localhost:3306 -db-user=root -db-password=root -db-schema=tododb
