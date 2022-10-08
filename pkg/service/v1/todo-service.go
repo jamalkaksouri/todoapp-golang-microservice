3fb7da896c42adb53260a6c0af974a7d571d39bf
Jamal Kaksouri <jamal.kaksouri@gmail.com>
2022-10-08 16:04:04 +0330

    Update todo-service.go

--- pkg/service/v1/todo-service.go
+++ pkg/service/v1/todo-service.go
@@ -181,7 +181,7 @@ func (s *toDoServiceServer) Update(ctx context.Context, req *v1.UpdateRequest) (
 	res, err := c.ExecContext(ctx, "UPDATE ToDo SET `Title`=?, `Description`=?, `Reminder`=? WHERE `ID`=?",
 		req.ToDo.Title, req.ToDo.Description, req.ToDo.Reminder.AsTime(), req.ToDo.Id)
 	if err != nil {
-		return nil, status.Error(codes.Unknown, "failed to update ToDo Data-> "+err.Error())
+		return nil, status.Error(codes.Unknown, "failed to update ToDo-> "+err.Error())
 	}
 
 	rows, err := res.RowsAffected()
